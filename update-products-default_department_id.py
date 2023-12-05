7a0adeb7b1978a0135127ecc13f9aa61c8e5d9e3
Mikkel Rask <35899030+mikkelrask@users.noreply.github.com>
2023-12-05 11:58:12 +0100

    default-department-id-script use api-lib

--- update-products-default_department_id.py
+++ update-products-default_department_id.py
@@ -1,74 +1,39 @@
-import csv
-import requests
-import sys
 import os
-import time
 from dotenv import load_dotenv
+from utils.api_utils import process_csv, print_results, send_request, FILE_PATH
 
 load_dotenv()
 
 BEARER_TOKEN = os.getenv("BEARER_TOKEN")
-
-errors = 0
-successes = 0
-failers = []
-
-
-# Save results as log file
-def log_results():
-    global errors, successes, failers
-    with open("update-products-default_location_id.log", "w") as f:
-        f.write("Errors: " + str(errors) + "\n")
-        f.write("Successes: " + str(successes) + "\n")
-        f.write("Failers: " + str(failers) + "\n")
+STATUS_CODES = {
+    404: "Product {0} was not found. Please check the product exists.",
+    422: "Product {0} could not be updated. Please check the department_id is valid.",
+}
 
 def update_product(row):
-    global errors, successes, failers
-    url = "https://app.rackbeat.com/api/products/" + row.get("number")
+    global FILE_PATH
+    if len(row) < 2:
+        print(f"CSV file {FILE_PATH} does not have the nessecary rows (number;department_id).")
+        exit()
+
+    url = "https://app.rackbeat.com/api/products/" + row[0]
     payload = {
-        "number": row.get("number"),
-        "department_id": row.get("default_location_id")
+        "number": row[0],
+        "department_id": row[1]
     }
     headers = {
-        "Authorization": "Bearer " + BEARER_TOKEN,
+        "Authorization": f"Bearer {BEARER_TOKEN}",
         "Content-Type": "application/json",
     }
-    response = requests.put(url, headers=headers, json=payload)
+    response = send_request(url, 'PUT', headers, payload)
     if response.status_code == 200:
-        print("Updated product: " + row.get("number"))
-        print("-----------------------")
-        successes = successes + 1
+        print(f"Updated product {row[0]}'s default department ID: {row[1]}")
     else:
-        print("Failed to update: " + row.get("number"))
-        print("Error message:", response.text)
-        print("-----------------------")
-        errors = errors + 1
-        failers.append(row.get("number"))
+        message = STATUS_CODES.get(response.status_code, "Product {0} could not be updated. Error: {1}")
+        print(message.format(row[0], response.status_code))
 
+    return response
 
-# Get the CSV file path from the command line argument
-csv_file = os.getenv("IMPORT_FILE")
-
-# Define the required fields in the CSV
-required_fields = ["number", "department_id"]
-
-# Iterate through the CSV file
-with open(csv_file, "r", newline="") as file:
-    csv_reader = csv.DictReader(file, delimiter=";")
-    for row in csv_reader:
-        # Check if the essential fields have non-empty values
-        if (
-            row.get("number")
-        ):
-            update_product(row)
-            time.sleep(0.125)  # Pause for 0.125 seconds (1/8th of a second) between requests
-        else:
-            print("Skipping row: Number is required")
-            print("-----------------------------------")
-
-print("")
-print("Update of product default location complete!")
-print("Total products updated: " + str(successes))
-print("Total products failed: " + str(errors))
-print("Failed products: " + str(failers))
-print("")
+if __name__ == "__main__":
+    process_csv(update_product)
+    print_results()
