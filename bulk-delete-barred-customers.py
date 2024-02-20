76f09ad1b3c98faeb42bc2128a479fc7671fb84c
Mikkel Rask <mikkelrask@pm.me>
2024-02-20 21:12:25 +0100

    Barred customers: You're outta here

--- bulk-delete-barred-customers.py
+++ bulk-delete-barred-customers.py
@@ -1,21 +1,21 @@
 from utils.api_utils import process_csv, delete_request, send_request, print_results
-customers
+
 def check_if_barred(row):
-    endpoint = f"customers/{row[0]}"
+    endpoint = f"customers/{row[0]}?fields=meta,is_barred"
     response = send_request(endpoint)
+    barred = response.json()['customer']['is_barred']
 
     if response.status_code == 200:
-        product = response.json()
-        if product['customers']['is_barred'] == True:
-            print(f"Customer {row[0]} is barred - deleting")
+        if barred == True:
+            print(f"☠️⚰️ Customer {row[0]} is barred - cya!")
             delete_request(endpoint)
         else:
-            print(f"Customer {row[0]} is not barred - skipping")
+            print(f"🍀 Customer {row[0]} is not barred - skipping")
         print("-----------------------")
     else:
         print(f"Failed to get customers {row[0]}")
         print("Status code:", response.status_code)
-        print("-----------------------")
+        print("")
 
 if __name__ == "__main__":
     process_csv(check_if_barred)
